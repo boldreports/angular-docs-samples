871f2ba9894b964b28c56003975d2ae0b8c171b0
Vishnu7101 <vishnu.tamilventhan@syncfusion.com>
2025-04-17 18:46:31 +0530

    feature(REPORT-425437): Bold Reports 9.1.7

--- src/app/common/app-routing.module.ts
+++ src/app/common/app-routing.module.ts
@@ -1,20 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { Type, ModuleWithProviders, NgModule } from '@angular/core';
-import { BoldReportsModule } from '@boldreports/angular-reporting-components';
 import { Routes, RouterModule } from '@angular/router';
 import { ReportViewerGettingStartedComponent } from '../components/report-viewer/getting-started/getting-started.component';
 import { ReportDesignerGettingStartedComponent } from '../components/report-designer/getting-started/getting-started.component';
-const components: any[] | Type<any> | ModuleWithProviders<{}> = [
+
+const components: any[] | Type<any> | ModuleWithProviders<{}> = [
   ReportViewerGettingStartedComponent,
   ReportDesignerGettingStartedComponent
 ];
-const routes: Routes = [
+
+const routes: Routes = [
   { path: '', redirectTo: 'report-viewer/getting-started', pathMatch: 'full' },
   { path: 'report-viewer/getting-started', component: ReportViewerGettingStartedComponent },
   { path: 'report-designer/getting-started', component: ReportDesignerGettingStartedComponent },
   { path: '**', redirectTo: 'report-viewer/getting-started' }
 ];
-@NgModule({
+
+@NgModule({
   declarations: components,
   imports: [
     BrowserModule,
